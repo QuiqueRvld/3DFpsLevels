db6484c0212326ed14577869504294b18a6e849e
QuiqueRvld <enricollado03@gmail.com>
2024-10-31 11:13:41 +0100

    TestPrueba

--- Assets/Scripts/Prueba.cs
+++ Assets/Scripts/Prueba.cs
@@ -5,7 +5,7 @@ public class Prueba : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //Comentario de prueba
     }
 
     // Update is called once per frame
